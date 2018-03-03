b653d7780ccba488a656e8cb9b21b48ee823714e
hosalt <hosalt@qq.com>
2018-03-03 14:34:55 +0800

    实验Vuex

---
+++ Vuex/app.js
@@ -0,0 +1,22 @@
+new Vue({
+  // state
+  data () {
+    return {
+      count: 0
+    }
+  },
+  // view
+  template: `
+    <div>{{ count }}</div>
+  `,
+  // actions
+  methods: {
+    increment () {
+      this.count++
+    }
+  }
+})
+//debugger;
+store.commit('increment')
+
+console.log(store.state.count) // -> 1
